12fdae3bf1930d34ecf8729592875497ec550301
Sergi Mansilla <sergi.mansilla@gmail.com>
2017-11-09 21:43:20 +0100

    First functional prototype

--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 import { hostname, tmpdir } from "os";
 import { mesos } from "./proto/messages";
-import * as fs from "fs";
-import * as http from "http";
+import { readFile, writeFile, writeFileSync } from "fs";
+import { join } from "path";
+import { request } from "http";
+
+import * as debug from "debug";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import * as request from "request";
 
-const util = require("util");
-const path = require("path");
-
+const log = debug("SimpleScheduler:log");
 // Files
-const stateFile = path.join(tmpdir(), "state.json");
-const frameworkInfoFile = path.join(tmpdir(), "framework.json");
+const stateFile = join(tmpdir(), "state.json");
+const frameworkInfoFile = join(tmpdir(), "framework.json");
 
 const Call = mesos.scheduler.Call;
 const PORT = 9090;
 
 // Url to Mesos master scheduler API
-const schedulerApiUrl = "http://10.10.10.10:5050/api/v1/scheduler";
+const SCHEDULER_API_URL = "http://AFSTP.local:5050/api/v1/scheduler";
 // Current framework configuration
 let frameworkInfo: mesos.FrameworkInfo;
 let mesosStreamID: string;
@@ -26,7 +26,7 @@ let taskID: number;
 
 const commandQueue: string[] = [];
 
-let tasksStatus: mesos.ITaskStatus;
+let tasksState: { [key: string]: mesos.ITaskStatus } = {};
 
 (function() {
   const user = "root";
@@ -38,35 +38,37 @@ let tasksStatus: mesos.ITaskStatus;
   const failoverTimeout = 3600;
   const checkpoint = true;
 
-  fs.readFile(frameworkInfoFile, (err, buf) => {
+  readFile(frameworkInfoFile, (err, buf) => {
     if (err) {
       frameworkInfo = new mesos.FrameworkInfo({
         user,
         name,
-        failoverTimeout,
+        failover_timeout: failoverTimeout,
         checkpoint,
         hostname: host,
-        webuiUrl
+        webui_url: webuiUrl
       });
-      return;
+    } else {
+      frameworkInfo = new mesos.FrameworkInfo(JSON.parse(buf.toString()));
     }
-    frameworkInfo = new mesos.FrameworkInfo(JSON.parse(buf.toString()));
+    log("Asd")
+    subscribe();
   });
 })();
 
 function kill(id, cb) {
-  if (!tasksStatus[id]) {
+  if (!tasksState[id]) {
     return cb(new Error(`Unknown task ${id}`));
   }
 
-  const update = tasksStatus[id];
+  const update = tasksState[id];
   console.log(`Kill task ${id} [${update}]"`);
 
   const killCall = new Call({
     type: Call.Type.KILL,
     kill: new Call.Kill({
-      taskId: update.taskId,
-      agentId: update.agentId
+      task_id: update.task_id,
+      agent_id: update.agent_id
     })
   });
 
@@ -75,84 +77,103 @@ function kill(id, cb) {
 
 function subscribe() {
   const subscribeCall = new Call({
-    frameworkId: frameworkInfo.id,
+    framework_id: frameworkInfo.id,
     type: Call.Type.SUBSCRIBE,
     subscribe: new Call.Subscribe({
-      frameworkInfo
+      framework_info: frameworkInfo
     })
   });
-
-  const body = JSON.stringify(subscribeCall.toJSON());
-  console.log(body);
-
+  const subscribeCallString = JSON.stringify(
+    subscribeCall.toJSON(),
+    undefined,
+    2
+  );
   const options = {
-    url: schedulerApiUrl,
+    hostname: "localhost",
+    port: 5050,
+    path: "/api/v1/scheduler",
+    method: "POST",
     headers: {
-      "Content-Type": "application/json"
-    },
-    body
-  };
-
-  request.post(options, (err, res, body) => {
-    if (err || res.statusCode !== 202) {
-      console.error(body);
-      return console.log(`Error ${res.statusCode}`);
+      "Content-Type": "application/json",
+      "Content-Length": subscribeCallString.length
     }
+  };
+  const Event = mesos.scheduler.Event;
+  const req = request(options, res => {
+    log(`STATUS: ${res.statusCode}`);
+    log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding("utf8");
+    res.on("data", parseChunk);
+    res.on("end", () => {
+      log("No more data in response.");
+    });
+    function parseChunk(body) {
+      const lines = body.split("\n");
+      let bytesCount = parseInt(lines.shift(), 10);
 
-    const lines = body.split("\n");
-    let bytesCount = parseInt(lines.shift(), 10);
+      while (true) {
+        const line = lines.shift();
+        const data = line.substring(0, bytesCount);
 
-    while (true) {
-      const line = lines.shift();
-      const data = line.substring(0, bytesCount);
-      bytesCount = line.substring(bytesCount);
-      if (bytesCount === 0) {
-        continue;
-      }
+        bytesCount = parseInt(line.substring(bytesCount), 10);
+        // Do not handle events, just log them
+        log("Received", JSON.stringify(JSON.parse(data)));
 
-      const Event = mesos.scheduler.Event;
-      // Do not handle events, just log them
-      console.log(`Got [${data}]`);
-      const event = new mesos.scheduler.Event(JSON.parse(data));
-      console.log(`Got [${event.toJSON().toString()}]`);
+        const event = new Event(JSON.parse(data));
+        const parsedEvent = event.toJSON();
 
-      const EventType = mesos.scheduler.Event.Type;
-      switch (event.type) {
-        case EventType.SUBSCRIBED:
-          console.log("Subscribed");
-          frameworkInfo.id = event.subscribed.frameworkId;
-          mesosStreamID = <string>res.headers["Mesos-Stream-Id"];
-        // ioutil.WriteFile(frameworkInfoFile, []byte(json), 0644)
-        // reconcile()
-        case EventType.HEARTBEAT:
-          console.log("PING");
-        case EventType.OFFERS:
-          console.log(`Handle offers returns: ${handleOffers(event.offers)}`);
-        case EventType.UPDATE:
-          console.log(`Handle update returns: ${handleUpdate(event.update)}`);
+        log(`Received event ${JSON.stringify(parsedEvent)}`);
+
+        switch (parsedEvent.type) {
+          case Event.Type.SUBSCRIBED:
+            log("Subscribed");
+            frameworkInfo.id = parsedEvent.subscribed.framework_id;
+            mesosStreamID = <string>res.headers["mesos-stream-id"];
+            writeFileSync(frameworkInfoFile, JSON.stringify(frameworkInfo), {
+              mode: 0o644
+            });
+            reconcile();
+            break;
+          case Event.Type.HEARTBEAT:
+            log("PING");
+            break;
+          case Event.Type.OFFERS:
+            log(`Handle offers returns: ${handleOffers(parsedEvent.offers)}`);
+            break;
+          case Event.Type.UPDATE:
+            log(`Handle update returns: ${handleUpdate(parsedEvent.update)}`);
+            break;
+        }
+
+        if (isNaN(bytesCount) || bytesCount === 0) {
+          break;
+        }
       }
     }
   });
+
+  req.write(subscribeCallString);
+  req.end();
 }
 
 function reconcile() {
-  fs.readFile(stateFile, (err, buf) => {
+  readFile(stateFile, (err, buf) => {
     if (!err) {
-      tasksStatus = JSON.parse(buf.toString());
+      tasksState = JSON.parse(buf.toString());
     }
 
     const oldTasks: mesos.scheduler.Call.Reconcile.Task[] = [];
     let maxId = 0;
 
-    Object.keys(tasksStatus).forEach(k => {
-      const t: mesos.ITaskStatus = tasksStatus[k];
+    Object.keys(tasksState).forEach(k => {
+      const t: mesos.ITaskStatus = tasksState[k];
       oldTasks.push(
         new mesos.scheduler.Call.Reconcile.Task({
-          taskId: t.taskId,
-          agentId: t.agentId
+          task_id: t.task_id,
+          agent_id: t.agent_id
         })
       );
-      const numericId = parseInt(t.taskId.value, 10);
+      const numericId = parseInt(t.task_id.value, 10);
       if (numericId > maxId) {
         maxId = numericId;
       }
@@ -170,9 +191,9 @@ function reconcile() {
 }
 
 function handleUpdate(update: mesos.scheduler.Event.IUpdate) {
-  tasksStatus[update.status.taskId.value] = update.status;
-  const stateJSON = JSON.stringify(tasksStatus);
-  fs.writeFile(
+  tasksState[update.status.task_id.value] = update.status;
+  const stateJSON = JSON.stringify(tasksState);
+  writeFile(
     stateFile,
     stateJSON,
     {
@@ -183,8 +204,8 @@ function handleUpdate(update: mesos.scheduler.Event.IUpdate) {
         new mesos.scheduler.Call({
           type: mesos.scheduler.Call.Type.ACKNOWLEDGE,
           acknowledge: new mesos.scheduler.Call.Acknowledge({
-            agentId: update.status.agentId,
-            taskId: update.status.taskId,
+            agent_id: update.status.agent_id,
+            task_id: update.status.task_id,
             uuid: update.status.uuid
           })
         })
@@ -202,10 +223,10 @@ function handleOffers(offers: mesos.scheduler.Event.IOffers) {
 
     const taskInfo = new mesos.TaskInfo({
       name: cmd,
-      taskId: new mesos.TaskID({
+      task_id: new mesos.TaskID({
         value: newTaskId.toString()
       }),
-      agentId: firstOffer.agentId,
+      agent_id: firstOffer.agent_id,
       resources: defaultResources(),
       command: new mesos.CommandInfo({
         shell: true,
@@ -213,48 +234,62 @@ function handleOffers(offers: mesos.scheduler.Event.IOffers) {
       })
     });
 
+    const Operation = mesos.Offer.Operation;
     const acceptCall = new Call({
       type: Call.Type.ACCEPT,
       accept: new Call.Accept({
-        offerIds,
+        offer_ids: offerIds,
         operations: [
-          new mesos.Offer.Operation({
-            type: mesos.Offer.Operation.Type.LAUNCH,
-            launch: new mesos.Offer.Operation.Launch({ taskInfos: [taskInfo] })
+          new Operation({
+            type: Operation.Type.LAUNCH,
+            launch: new Operation.Launch({ task_infos: [taskInfo] })
           })
         ]
       })
     });
+
     return call(acceptCall);
   } else {
     return call(
       new Call({
         type: Call.Type.DECLINE,
-        decline: new Call.Decline({ offerIds })
+        decline: new Call.Decline({ offer_ids: offerIds })
       })
     );
   }
 }
 
 function call(message: mesos.scheduler.Call) {
-  message.frameworkId = frameworkInfo.id;
+  message.framework_id = frameworkInfo.id;
 
-  const body = JSON.stringify(message.toJSON());
   const options = {
-    url: schedulerApiUrl,
+    hostname: "localhost",
+    port: 5050,
+    path: "/api/v1/scheduler",
+    method: "POST",
     headers: {
       "Mesos-Stream-Id": mesosStreamID,
       "Content-Type": "application/json"
-    },
-    body
+    }
   };
 
-  request.post(options, (err, res, body) => {
-    if (err || res.statusCode !== 202) {
-      console.error(body);
-      return console.log(`Error ${res.statusCode}`);
-    }
+  const req = request(options, res => {
+    log(`STATUS: ${res.statusCode}`);
+    log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding("utf8");
+    res.on("data", body => {
+      if (res.statusCode !== 202) {
+        console.error(body);
+        return log(`Error ${res.statusCode}`);
+      }
+    });
+    res.on("end", () => {
+      log("No more data in response.");
+    });
   });
+
+  req.write(JSON.stringify(message.toJSON()));
+  req.end();
 }
 
 function defaultResources(): mesos.Resource[] {
@@ -278,7 +313,6 @@ function defaultResources(): mesos.Resource[] {
 
 const app = express();
 
-// Tell express to use the body-parser middleware and to not parse extended bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/", function(req, res) {
@@ -293,14 +327,14 @@ app.delete("/", function(req, res) {
     if (err) {
       res.send(err);
     } else {
-      console.log(`KILLED ${id}`);
+      log(`KILLED ${id}`);
     }
   });
 });
 
 app.get("/", (req, res) => {
   res.set("Content-Type", "application/json");
-  res.json(tasksStatus);
+  res.json(tasksState);
 });
 
 app.listen(PORT, function(err) {
@@ -308,5 +342,5 @@ app.listen(PORT, function(err) {
     throw err;
   }
 
-  console.log(`Server started on port ${PORT}`);
+  log(`Server started on port ${PORT}`);
 });
